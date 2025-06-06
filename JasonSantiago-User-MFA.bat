62fb1025812bcdf638f39b9218ad21c390e6da0a
Jason Santiago aka Cyber-Associate | Lifetime Learner <122810889+jsanti1975on@users.noreply.github.com>
2025-06-06 11:05:32 -0400

    Create JasonSantiago-User-MFA.bat

---
+++ JasonSantiago-User-MFA.bat
@@ -0,0 +1,57 @@
+@echo off
+:: Security+ MFA Self-Check and Logging Script
+title MFA Self-Check Logger
+
+:: Identity Check
+set /p usercheck=Are you Jason Santiago? [yes/no]: 
+if /i not "%usercheck%"=="yes" (
+    echo You are not authorized to run this script.
+    pause
+    exit /b
+)
+
+:: Get timestamp
+for /f "tokens=1-4 delims=/ " %%a in ("%date%") do set mydate=%%a-%%b-%%c
+for /f "tokens=1-2 delims=: " %%a in ("%time%") do set mytime=%%a_%%b
+
+:: Set custom log folder and ensure it exists
+set "logfolder=C:\Users\redacted\Documents\_Bakup2024-2025\MFA_Log"
+if not exist "%logfolder%" (
+    mkdir "%logfolder%"
+)
+
+:: Set log file path
+set "logfile=%logfolder%\MFA_Log_%mydate%_%mytime%.txt"
+
+:: Begin log
+echo Security+ MFA Self-Check >> "%logfile%"
+echo Username: %USERNAME% >> "%logfile%"
+echo Timestamp: %date% %time% >> "%logfile%"
+echo. >> "%logfile%"
+
+:: Prompt user for MFA components
+set /p know=Do you have something you know? (e.g., password) [yes/no]: 
+echo Something you know: %know% >> "%logfile%"
+if /i not "%know%"=="yes" goto denied
+
+set /p have=Do you have something you have? (e.g., phone or token) [yes/no]: 
+echo Something you have: %have% >> "%logfile%"
+if /i not "%have%"=="yes" goto denied
+
+set /p are=Do you have something you are? (e.g., fingerprint or face ID) [yes/no]: 
+echo Something you are: %are% >> "%logfile%"
+if /i not "%are%"=="yes" goto denied
+
+echo MFA Self-Check Passed >> "%logfile%"
+echo Launching secure Microsoft sign-in portal...
+start https://mysignins.microsoft.com/security-info
+goto end
+
+:denied
+echo MFA Self-Check Failed >> "%logfile%"
+echo Secure portal will not be launched.
+goto end
+
+:end
+echo Log saved to: %logfile%
+pause
